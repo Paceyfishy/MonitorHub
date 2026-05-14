e16fd601c34d14c1496f1d57e7f3c95be24510d6
Patteera S. <patteera@mbp13.local>
2026-05-14 15:55:39 +0700

    profile name

--- constants/monitors.ts
+++ constants/monitors.ts
@@ -23,4 +23,39 @@ export const allMonitors = [
     price: "$699",
     image: "https://dummyimage.com/300x300/cccccc/000000.png&text=Samsung",
   },
+  {
+    id: "5",
+    name: "LG UltraGear 27GN800",
+    price: 10990,
+    image:
+      "https://www.lg.com/content/dam/channel/wcms/th/images/monitors/27gn800-b_atr_eath_th_c/gallery/desktop-01.jpg",
+  },
+  {
+    id: "6",
+    name: "ASUS TUF Gaming VG249Q1A",
+    price: 6490,
+    image:
+      "https://dlcdnwebimgs.asus.com/gain/5A1A58F4-7B1D-4D69-A62F-2D1F6C90E9A7",
+  },
+  {
+    id: "7",
+    name: "Acer Nitro XV272U",
+    price: 11990,
+    image:
+      "https://static.acer.com/up/Resource/Acer/Monitors/Nitro_XV2/Product_Gallery/20200818/Acer-Nitro-XV272U-KV-Black-gallery-01.png",
+  },
+  {
+    id: "8",
+    name: "Dell S2721DGF",
+    price: 13990,
+    image:
+      "https://i.dell.com/sites/imagecontent/products/PublishingImages/dell-s2721dgf-monitor/",
+  },
+  {
+    id: "9",
+    name: "Gigabyte M27Q",
+    price: 12990,
+    image:
+      "https://static.gigabyte.com/StaticFile/Image/Global/monitor-m27q-gallery-01.png",
+  },
 ];
